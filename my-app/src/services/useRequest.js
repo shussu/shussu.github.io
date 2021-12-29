37b01032ae64814171901df367e191c5d3379dee
shussu <agathang93@gmail.com>
2021-12-29 00:21:31 -0800

    Add github page config

---
+++ my-app/src/services/useRequest.js
@@ -0,0 +1,54 @@
+import { useQuery } from "react-query";
+import { GraphQLClient, gql } from "graphql-request";
+
+const ENDPOINT = 'https://api-us-west-2.graphcms.com/v2/ckxe0tkow4ifv01xi9ms69zca/master';
+
+const graphQLClient = new GraphQLClient(ENDPOINT, {
+    headers: {
+      authorization: '',
+    },
+})
+
+const getPostsQuery = gql`
+    query {
+        posts {
+            id
+            title
+            content
+            preview
+            createdAt
+        }
+    }
+`;
+
+export function useGetPosts() {
+    return useQuery("get-posts",  async () => {
+        const  getPostList = await graphQLClient.request(getPostsQuery)
+        console.debug('useGetPosts response: ', getPostList);
+        return getPostList;
+      }
+    );
+}
+
+const getPostByIdQuery = gql`
+    query getPost($id: ID!) {
+        post(where: {id: $id}) {
+            id
+            content
+            title
+            preview
+        }
+    }
+`;
+
+export function useGetPost(postId) {
+    console.debug("Post Id: ", postId);
+    return useQuery(["get-post", postId], async () => {
+      const getPost = await graphQLClient.request(
+        getPostByIdQuery,
+        { id: postId }
+      );
+      console.debug("useGetPost response: ", getPost);
+      return getPost;
+    });
+  }
